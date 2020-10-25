5edc73007d948b750dc76643275722ccff07a2df
alexey <info@sopdu.org>
2020-10-25 12:30:41 +0300

    add: max number, levels

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
+max_number = int(input('Введите максимально возможное загаданное число: '))
 import random
-number = random.randint(1, 100)
+number = random.randint(1, max_number)
+print('Вы угадываете число от 1 до ' + str(max_number))
+user_number = None
+count = 0
+levels = {
+    1: 10,
+    2: 5,
+    3: 3
+}
+level = int(input('выберите уровень сложности (от 1 до 3): '))
+max_count = levels[level]
 
-while True:
-    user_number = int(input('введите число: '))
-    if number == user_number:
-        print('Победа! Вы угадали!')
+while number != user_number:
+    count += 1
+    if count > max_count:
+        print('Вы проиграли')
         break
-    elif number > user_number:
+    print(f'Попытка № {count}')
+    user_number = int(input('введите число: '))
+    if number < user_number:
         print('Ваше число больше загадоного')
-    else:
+    elif number > user_number:
         print('Ваше число меньше загадоного')
+else:
+    print('Победа! Вы угадали!')
