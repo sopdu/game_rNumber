869935e8493a7e4fda1c24fcc77ed1feb452490c
alexey <info@sopdu.org>
2020-10-25 13:11:28 +0300

    обработка ошибок

--- main.py
+++ main.py
@@ -9,19 +9,26 @@ levels = {
     2: 5,
     3: 3
 }
+print("Сложность 1 - 10 попыток")
+print("Сложность 2 - 5 попыток")
+print("Сложность 3 - 3 попыток")
 level = int(input('выберите уровень сложности (от 1 до 3): '))
-max_count = levels[level]
-
-while number != user_number:
-    count += 1
-    if count > max_count:
-        print('Вы проиграли')
-        break
-    print(f'Попытка № {count}')
-    user_number = int(input('введите число: '))
-    if number < user_number:
-        print('Ваше число больше загадоного')
-    elif number > user_number:
-        print('Ваше число меньше загадоного')
+while level > 3 or level < 1:
+    print('Надо выбрать сложность от 1 до 3')
+    level = int(input('выберите уровень сложности (от 1 до 3): '))
 else:
-    print('Победа! Вы угадали!')
+    max_count = levels[level]
+
+    while number != user_number:
+        count += 1
+        if count > max_count:
+            print('Вы проиграли')
+            break
+        print(f'Попытка № {count}')
+        user_number = int(input('введите число: '))
+        if number < user_number:
+            print('Ваше число больше загадоного')
+        elif number > user_number:
+            print('Ваше число меньше загадоного')
+    else:
+        print('Победа! Вы угадали!')
